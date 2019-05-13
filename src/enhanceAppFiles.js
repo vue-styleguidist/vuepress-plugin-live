6a10ca87dd879b3f9c2d0d562ada29f3dd604de9
elevatebart <ledouxb@me.com>
2019-05-13 14:46:51 -0500

    make it almost work

--- src/enhanceAppFiles.js
+++ src/enhanceAppFiles.js
@@ -1,7 +1,8 @@
 module.exports = function enhanceAppFiles() {
   const content = `const Vue = require('vue').default;
-  // for some reason, buble needs a window.global variable
-  window.global = window;
+  if(typeof window !== "undefined"){
+    window.global = {}
+  }
   const { VueLive } = require('vue-live');
   Vue.component('vue-live', VueLive);`;
   return { name: "vue-live-enhancer", content };
