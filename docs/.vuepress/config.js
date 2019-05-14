e284ea861b47eaeeb88b430c3b113c92e75d0468
elevatebart <ledouxb@me.com>
2019-05-14 09:41:18 -0500

    docs: add link to github to the vuepress site

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,8 @@ module.exports = {
   title: "VuePress Live",
   description: "Demo site of the VuePress-live plugin",
   themeConfig: {
+    repo: "vue-styleguidist/vuepress-live",
+    editLinks: true,
     docsDir: "docs"
   },
   plugins: [[require("../../src/index")]]
