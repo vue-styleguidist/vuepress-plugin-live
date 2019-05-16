a95f23cebaf9343a60a3aaaf2ec098bce75cc639
Barthelemy Ledoux <bart.ledoux@enablon.com>
2019-05-16 13:58:34 -0500

    fix: loading of external components

--- src/enhanceAppFiles.js
+++ src/enhanceAppFiles.js
@@ -1,15 +1,12 @@
 const path = require("path");
-
+const vueLivePath = path.join(__dirname, "./vueLiveWithLayout");
 module.exports = function enhanceAppFiles() {
   const content = [
     // import live with es6 so that it is compatible with both build and dev
     'import Vue from "vue";',
     // and in order for the loading to happen after we assign the variable,
     // we load vue-live as a require instead of an import
-    `import VueLive from ${JSON.stringify(
-      path.join(__dirname, "./vueLiveWithLayout")
-    )};`,
-    "Vue.component('vue-live', VueLive);"
+    `Vue.component("VueLive", () => import(${JSON.stringify(vueLivePath)}))`
   ].join("\n");
   return { name: "vue-live-enhancer", content };
 };
