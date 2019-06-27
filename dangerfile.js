bed03a47df9ab6278b19a9d3b44114fec2403d40
Barthelemy Ledoux <bart.ledoux@enablon.com>
2019-06-27 08:40:56 -0500

    ci: update dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -8,7 +8,7 @@ const lockfileChanged = danger.git.modified_files.includes("package-lock.json");
 
 if (packageChanged && !lockfileChanged) {
   warn(`Changes were made to \`package.json\`, but not to \`package-lock.json\`.
-If you’ve changed any dependencies (added, removed or updated any packages), please run \`yarn install\` and commit changes in package-lock.json file.`);
+If you’ve changed any dependencies (added, removed or updated any packages), please run \`npm install\` and commit changes in package-lock.json file.  Make sure you’re using npm 5.7+.`);
 }
 
 if (!packageChanged && lockfileChanged) {
