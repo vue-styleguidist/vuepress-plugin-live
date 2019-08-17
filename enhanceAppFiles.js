1b5cfa27dca75faa690985c4eeb3df6dd679c799
elevatebart <ledouxb@me.com>
2019-08-17 18:25:36 -0500

    fix: move polyfill to enhancer to allow import

--- enhanceAppFiles.js
+++ enhanceAppFiles.js
@@ -1,9 +1,17 @@
 const path = require("path");
 const vueLivePath = path.join(__dirname, "./vueLiveWithLayout");
+
 module.exports = function enhanceAppFiles() {
   const content = [
     // import live with es6 so that it is compatible with both build and dev
     'import Vue from "vue";',
+    // vuepress is compiled in both node and non-node models
+    // it needs a "global" variable to make buble work.
+    // one of its dependencies, buffer uses global to detect compat with browser
+    // it is missing in browsers, so we add it.
+    'if (typeof window !== "undefined" && window.global === undefined) {',
+    "  window.global = window;",
+    "}",
     // and in order for the loading to happen after we assign the variable,
     // we load vue-live as a require instead of an import
     `Vue.component("VueLive", () => import(${JSON.stringify(vueLivePath)}))`
--- vueLiveWithLayout.js
+++ vueLiveWithLayout.js
@@ -1,12 +1,5 @@
 import layout from "vuepress-plugin-live/live-layout";
-
-// vuepress is compiled in both node and non-node models
-// it needs a "global" variable to make buble work.
-// it is missing in browsers, so we add it.
-if (typeof window !== "undefined" && window.global === undefined) {
-  window.global = {};
-}
-const { VueLive } = require("vue-live");
+import { VueLive } from "vue-live";
 
 export default {
   functional: true,
