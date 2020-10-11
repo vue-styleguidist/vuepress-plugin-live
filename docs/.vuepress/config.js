b2dc7ce8b1f39a6e08489a5906a379fada4ab66d
ElevateBart <ledouxb@gmail.com>
2020-10-11 18:43:50 -0500

    docs: more comments

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,8 @@ module.exports = {
         // uncomment next line to use a custom layout
         // layout: path.resolve(__dirname, "./components/custom-layout.vue"),
 
-        // I chose to show a component that can't be server side rendered
+        // I chose to show a component, vue-slider,
+        // that can't be server side rendered
         // so I use the noSsr flag to avoid the "document is not defined" error
         noSsr: true,
       },
